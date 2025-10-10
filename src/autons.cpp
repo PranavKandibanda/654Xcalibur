2ec05bb0e9b207e1389823cc0bb22664af80c7b2
Pranav K <pkandibanda123@gmail.com>
2025-10-10 16:34:26 -0500

    blueLeftWinpoint fully tuned and working

--- src/autons.cpp
+++ src/autons.cpp
@@ -10,10 +10,10 @@ void default_constants(void) {
     chassis.set_control_constants(5, 10, 1.019, 5, 10, 1.019);
 
     // Each constant set is in the form of (maxVoltage, kP, kI, kD, startI).
-    chassis.set_turn_constants(12, .247, .011, 1.586, 15);
-    chassis.set_drive_constants(12, 1.3, 0.02, 7.5, 3);
+    chassis.set_turn_constants(12, .258, .004, 2.196, 45);
+    chassis.set_drive_constants(12, 1.2, 0.04, 7.7, 3);
     chassis.set_heading_constants(6, .3, .1, 3, 0);
-    chassis.set_swing_constants(12, .337, .0095, 3.486, 15);
+    chassis.set_swing_constants(12, .337, .0095, 3.486, 45);
 
     // Each exit condition set is in the form of (settle_error, settle_time, timeout).
     chassis.set_turn_exit_conditions(1.5, 75, 2000);
@@ -43,6 +43,7 @@ void unload(int height)
         if (height == 2)
         {
             intakeAssembly.front_intake_motor.spin(reverse, 12, volt);
+            
         }
     }
     else
@@ -73,10 +74,20 @@ void stopping(void)
 void odom_constants(void) {
     default_constants();
     chassis.heading_max_voltage = 10;
+    chassis.turn_min_voltage = 3;
     chassis.drive_max_voltage = 8;
     chassis.drive_settle_error = 3;
     chassis.boomerang_lead = .5;
-    chassis.boomerang_setback = 2;    
+    chassis.boomerang_setback = 2;
+    chassis.set_turn_constants(12, .258, .004, 2.196, 45);
+    chassis.set_drive_constants(12, 1.2, 0.09, 7.7, 3);
+    chassis.set_heading_constants(6, .3, .1, 3, 0);
+    chassis.set_swing_constants(12, .337, .0095, 3.486, 45);
+
+    // Each exit condition set is in the form of (settle_error, settle_time, timeout).
+    chassis.set_turn_exit_conditions(1.5, 75, 2000);
+    chassis.set_drive_exit_conditions(1, 75, 3000);
+    chassis.set_swing_exit_conditions(1.25, 75, 3000);
 }
 
 std::string template_auto(bool calibrate, auto_variation var, bool get_name) {
@@ -127,20 +138,20 @@ std::string template_auto_other_variation(bool calibrate, bool get_name) {
 std::string blue_left_winpoint(bool calibrate, auto_variation var, bool get_name) {
     if (get_name) { return "blue left winpoint"; }
     if (calibrate) {
-        chassis.set_coordinates(53.843, -16.374, 180); //try 180 later when testing
+        //chassis.set_coordinates(29.039,-47.338, 90);
+        chassis.set_coordinates(53.99, -16.374, 180); //try 180 later when testing
         return "";
     }
     odom_constants();
-
-    chassis.drive_to_point(53.99,-47.338);
-    chassis.turn_to_point(62.79,-47.338);
-    chassis.drive_to_point(65.79,-47.338);
+    chassis.drive_to_point(53.99,-47.338,{.max_voltage = 6});
+    chassis.turn_to_point(62.702,-47.338);
     scraper.toggle();
-    scraper_mech(10);
+    scraper_mech(5);
     stopping();
     scraper.toggle();
     chassis.drive_to_point(29.039,-47.338);
     unload(3);
+    wait(3,sec);
     stopping();
     chassis.drive_to_point(38.61,-47.338);
     chassis.turn_to_point(22.545,-22.729);
@@ -150,6 +161,7 @@ std::string blue_left_winpoint(bool calibrate, auto_variation var, bool get_name
     chassis.turn_to_point(10.241,-10.425,{.angle_offset = 180});
     chassis.drive_to_point(10.241,-10.425);
     unload(2);
+    wait(2,sec);
     return "";
 }
 std::string blue_left_sawp(bool calibrate, auto_variation var, bool get_name) { 
