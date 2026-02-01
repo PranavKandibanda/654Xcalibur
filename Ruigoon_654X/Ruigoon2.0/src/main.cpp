39b34c3501458795c39257525c51230cf6498c5d
Pranav K <pkandibanda123@gmail.com>
2026-02-01 12:14:05 -0600

    Semi finished auton for left side

--- Ruigoon_654X/Ruigoon2.0/src/main.cpp
+++ Ruigoon_654X/Ruigoon2.0/src/main.cpp
@@ -2,6 +2,11 @@
 #include "lemlib/api.hpp" // IWYU pragma: keep
 #include "api.h" // IWYU pragma: keep
 #include "lemlib/chassis/trackingWheel.hpp"
+#include "pros/adi.hpp"
+#include "pros/distance.hpp"
+#include "pros/motor_group.hpp"
+#include "pros/rtos.hpp"
+#include <cstdlib>
 
 // controller
 pros::Controller controller(pros::E_CONTROLLER_MASTER);
@@ -9,9 +14,33 @@ pros::Controller controller(pros::E_CONTROLLER_MASTER);
 // motor groups
 pros::MotorGroup leftMotors({-6, -5, -4},pros::MotorGearset::blue); // left motor group - ports 3 (reversed), 4, 5 (reversed)
 pros::MotorGroup rightMotors({8, 9, 10}, pros::MotorGearset::blue); // right motor group - ports 6, 7, 9 (reversed)
-
+pros::MotorGroup intake({3,-7}, pros::MotorGearset::blue);
 // Inertial Sensor on port 10
-pros::Imu imu(10);
+pros::Imu imu(21);
+pros::Distance east(16);
+pros::Distance north(2);
+pros::Distance west(1);
+pros::Distance south(11);
+
+// Pneumatics
+pros::adi::Pneumatics hook_piston('G',false);
+pros::adi::Pneumatics scraper_piston('H',false);
+
+pros::adi::Pneumatics loading_piston('B',false);
+pros::adi::Pneumatics scoring_piston('C',false);
+
+constexpr double FIELD_SIZE_MM  = 3657.6/25.4;
+// Positive values = outward from center
+// Measured from tracking center to sensor (mm)
+// +X = east, +Y = north
+constexpr double NORTH_Y_OFFSET_MM = 228.6/25.4;
+constexpr double SOUTH_Y_OFFSET_MM = 101.6/25.4;
+constexpr double EAST_X_OFFSET_MM  = 152.4/25.4;
+constexpr double WEST_X_OFFSET_MM  = 152.4/25.4;
+
+
+// Distance from robot center to the distance sensor face
+constexpr double ROBOT_RADIUS_MM = 180.0; // adjust to your robot
 
 // drivetrain settings
 lemlib::Drivetrain drivetrain(&leftMotors, // left motor group
@@ -23,21 +52,21 @@ lemlib::Drivetrain drivetrain(&leftMotors, // left motor group
 );
 
 // lateral motion controller
-lemlib::ControllerSettings linearController(8, // proportional gain (kP)
-                                            0, // integral gain (kI)
-                                            3, // derivative gain (kD)
+lemlib::ControllerSettings linearController(14, // proportional gain (kP)
+                                            .01, // integral gain (kI)
+                                            240, // derivative gain (kD)
                                             3, // anti windup
-                                            1, // small error range, in inches
+                                            4, // small error range, in inches
                                             100, // small error range timeout, in milliseconds
-                                            3, // large error range, in inches
-                                            500, // large error range timeout, in milliseconds
-                                            20 // maximum acceleration (slew)
+                                            6, // large error range, in inches
+                                            300, // large error range timeout, in milliseconds
+                                            6 // maximum acceleration (slew)
 );
 
 // angular motion controller
 lemlib::ControllerSettings angularController(4.1, // proportional gain (kP)
                                              .18, // integral gain (kI)
-                                             21, // derivative gain (kD)
+                                             30, // derivative gain (kD)
                                              10, // anti windup
                                              1, // small error range, in degrees
                                              100, // small error range timeout, in milliseconds
@@ -92,13 +121,12 @@ void initialize() {
     // thread to for brain screen and position logging
     pros::Task screenTask([&]() {
         while (true) {
-            // print robot location to the brain screen
-            pros::lcd::print(0, "X: %f", chassis.getPose().x); // x
-            pros::lcd::print(1, "Y: %f", chassis.getPose().y); // y
 
-            printf("X: %f", chassis.getPose().x); // x
-            printf("Y: %f", chassis.getPose().y); // y
-            pros::lcd::print(2, "Theta: %f", chassis.getPose().theta); // heading
+            //printf("X: %f", chassis.getPose().x); // x
+            //printf("Y: %f", chassis.getPose().y); // y
+            pros::lcd::print(2, "X: %f", chassis.getPose().x); // heading
+            pros::lcd::print(3, "Y: %f", chassis.getPose().y); // heading
+            pros::lcd::print(4, "Theta: %f", chassis.getPose().theta); // heading
             // log position telemetry
             lemlib::telemetrySink()->info("Chassis pose: {}", chassis.getPose());
             // delay to save resources
@@ -124,9 +152,50 @@ void competition_initialize() {}
  *
  * This is an example autonomous routine which demonstrates a lot of the features LemLib has to offer
  */
+
+double normalizeAngle(double deg) {
+  while (deg < 0) deg += 360;
+  while (deg >= 360) deg -= 360;
+  return deg;
+}
+
+void distanceResetPose() {
+  double thetaDeg = chassis.getPose().theta;
+  thetaDeg = normalizeAngle(thetaDeg);
+
+  auto pose = chassis.getPose();
+  double x = pose.x;
+  double y = pose.y;
+
+  // Facing EAST (wall at +X)
+  if (thetaDeg >= 315 || thetaDeg < 45) {
+    double d = east.get_distance()/25.4;
+    x = FIELD_SIZE_MM - d - EAST_X_OFFSET_MM;
+  }
+  // Facing NORTH (wall at +Y)
+  else if (thetaDeg >= 45 && thetaDeg < 135) {
+    double d = east.get_distance()/25.4;
+    y = -(FIELD_SIZE_MM/2 - d - EAST_X_OFFSET_MM);
+  }
+  // Facing WEST (wall at 0)
+  else if (thetaDeg >= 135 && thetaDeg < 225) {
+    double d = west.get_distance()/25.4;
+    double d2 = north.get_distance()/25.4;
+    x = FIELD_SIZE_MM/2 - d - WEST_X_OFFSET_MM;
+    y = -(FIELD_SIZE_MM/2 - d2 - NORTH_Y_OFFSET_MM);
+    //pros::lcd::print(5, "West distance: %f", y);
+  }
+  // Facing SOUTH (wall at 0)
+  else {
+    double d = south.get_distance()/25.4;
+    y = 0.0 - d - SOUTH_Y_OFFSET_MM;
+  }
+  chassis.setPose(x, y, thetaDeg);
+}
+
 void autonomous() {
     chassis.setPose(0,0,0);
-    pros::Task desmosTask([&]() {
+    /*pros::Task desmosTask([&]() {
         // configure desmos logger
         while(true){
             // log position to desmos
@@ -134,21 +203,91 @@ void autonomous() {
             // delay to save resources
             pros::delay(50);
         }
-    });
-    chassis.moveToPoint(0,6,1000,{},false);
+    });*/
+    /*chassis.moveToPoint(0,6,1000,{},false);
     chassis.moveToPoint(0,18,1000,{},false);
     chassis.moveToPoint(0, 24, 1000,{},false); // move to (0,24) with a timeout of 1000ms
-    chassis.moveToPoint(0,0,1000,{.forwards = false},false);
+    chassis.moveToPoint(0,0,1000,{.forwards = false},false);*/
 
-    /*chassis.setPose(0,0,0);
+    chassis.setPose(0,0,0);
     chassis.turnToHeading(45,999,{},false);
     chassis.turnToHeading(90,999,{},false);
     //chassis.turnToHeading(135,999,{},false);
     chassis.turnToHeading(180,999,{},false);
     chassis.turnToHeading(270,999,{},false);
     chassis.turnToHeading(0,999,{},false);
-    chassis.turnToHeading(180,999,{},false);*/
+    chassis.turnToHeading(180,999,{},false);
     
+}
+
+void score()
+{
+    scoring_piston.set_value(false);
+    loading_piston.set_value(false);
+    intake.move(127);
+}
+
+void outtake()
+{
+    scoring_piston.set_value(false);
+    loading_piston.set_value(false);
+    intake.move(-127);
+}
+
+void load()
+{
+    scoring_piston.set_value(true);
+    loading_piston.set_value(true);
+    intake.move(127);
+}
+
+void middle()
+{
+    scoring_piston.set_value(false);
+    loading_piston.set_value(true);
+    intake.move(127);
+}
+
+void score_setup()
+{
+  scoring_piston.set_value(false);
+  loading_piston.set_value(false);
+}
+
+void four_3_left()
+{
+    scraper_piston.set_value(false);
+    chassis.setPose(0,0,180);
+    distanceResetPose();
+    chassis.setPose(chassis.getPose().x,-14,180);
+
+    chassis.moveToPoint(46.5, -46,1000, {},false);
+    scraper_piston.set_value(true);
+    load();
+
+    chassis.turnToHeading(90, 1000,{},false);
+    distanceResetPose();
+    chassis.arcade(80, 0);
+    pros::Task::delay(1000);
+
+    chassis.moveToPoint(29.7+2, -47.5, 1000, {.forwards = false},false);
+    score();
+    pros::delay(1000);
+
+    scraper_piston.set_value(false);
+    chassis.setPose(15,-48,90);
+
+    chassis.moveToPoint(22, -48, 1000, {}, false);
+    load();
+
+    chassis.moveToPose(21, -24-2, -50, 1000,{},false);
+    pros::delay(500);
+
+    chassis.arcade(60, 0);
+    pros::delay(1000);
+    chassis.turnToPoint(0,0,1000,{.forwards = false},false);
+    chassis.moveToPose(10, -10, 135+180-15, 1000,{.forwards=false}, false);
+    score();
 }
 
 /**
@@ -157,7 +296,8 @@ void autonomous() {
 void opcontrol() {
     // controller
     // loop to continuously update motors
-    autonomous();
+    //autonomous();
+    four_3_left();
     while (true) {
         // get joystick positions
         int leftY = controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
@@ -166,5 +306,45 @@ void opcontrol() {
         chassis.arcade(leftY, rightX);
         // delay to save resources
         pros::delay(10);
+
+        if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_DOWN))
+        {
+            scraper_piston.toggle();
+            pros::delay(200);
+        }
+        if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_B))
+        {
+            hook_piston.set_value(false);
+        }
+        if(controller.get_digital(pros::E_CONTROLLER_DIGITAL_L1)){
+            score();
+        }
+        else if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_L2)){
+            outtake();
+        }
+        else if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_R1))
+        {
+            load();
+        }
+        else if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_R2))
+        {
+            middle();
+        }
+        else {
+            intake.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+            intake.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+            intake.brake();
+            intake.brake();
+        }
+        if (controller.get_digital_new_release(pros:: E_CONTROLLER_DIGITAL_R2))
+        {
+            scoring_piston.set_value(true);
+            loading_piston.set_value(true);
+        }
+        else if(controller.get_digital_new_release(pros::E_CONTROLLER_DIGITAL_B))
+        {
+            hook_piston.set_value(true);
+            pros::delay(200);
+        }
     }
 }
